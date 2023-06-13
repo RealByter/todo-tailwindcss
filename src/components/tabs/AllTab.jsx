0271c111b1030107325d13bd3a88e76323a0bef3
RealByter <justavishay@gmail.com>
2023-06-13 23:55:04 +0300

    feat: implement the all tab component

--- src/components/tabs/AllTab.jsx
+++ src/components/tabs/AllTab.jsx
@@ -1,5 +1,24 @@
+import { useContext } from "react";
+import TodoContext from "../../ToDoContext";
+import ItemInput from "../ItemInput";
+import Item from "../Item";
+
 const AllTab = () => {
-  return <div>all</div>;
+  const { activeList, completedList } = useContext(TodoContext);
+
+  return (
+    <div>
+      <ItemInput />
+      <div className="mt-6 flex flex-col gap-4">
+        {activeList.map((item, index) => (
+          <Item key={index} completed={false} index={index} details={item} />
+        ))}
+        {completedList.reverse().map((item, index) => (
+          <Item key={index} completed={true} index={index} details={item} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AllTab;
