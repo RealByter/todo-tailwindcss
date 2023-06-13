d1f19cd22eb1252a7ff8c107a26cfd6a5135bf13
RealByter <justavishay@gmail.com>
2023-06-14 00:24:57 +0300

    feat: complete the completed tab component

--- src/components/tabs/CompletedTab.jsx
+++ src/components/tabs/CompletedTab.jsx
@@ -1,5 +1,27 @@
+import { useContext } from "react";
+import TodoContext from "../../ToDoContext";
+import Item from "../Item";
+import DeleteAllButton from "../DeleteAllButton";
+
 const CompletedTab = () => {
-  return <div>completed</div>;
+  const { completedList } = useContext(TodoContext);
+
+  return (
+    <>
+      <div className="flex flex-col gap-4">
+        {completedList.reverse().map((item, index) => (
+          <Item
+            key={index}
+            completed={true}
+            index={index}
+            details={item}
+            deletable
+          />
+        ))}
+      </div>
+      {completedList.length > 0 && <DeleteAllButton />}
+    </>
+  );
 };
 
 export default CompletedTab;
