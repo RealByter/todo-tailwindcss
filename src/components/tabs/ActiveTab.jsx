66779b64f47dd70c3e9d77283c8895c2eecacba2
RealByter <justavishay@gmail.com>
2023-06-13 23:55:14 +0300

    feat: complete the active tab component

--- src/components/tabs/ActiveTab.jsx
+++ src/components/tabs/ActiveTab.jsx
@@ -1,5 +1,21 @@
+import { useContext } from "react";
+import TodoContext from "../../ToDoContext";
+import ItemInput from "../ItemInput";
+import Item from "../Item";
+
 const ActiveTab = () => {
-  return <div>active</div>;
+  const { activeList } = useContext(TodoContext);
+
+  return (
+    <div>
+      <ItemInput />
+      <div className="mt-6 flex flex-col gap-4">
+        {activeList.map((item, index) => (
+          <Item key={index} completed={false} index={index} details={item} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ActiveTab;
